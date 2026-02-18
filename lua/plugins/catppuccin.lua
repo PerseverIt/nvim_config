d6240f7e04a5e766330fa1f11bc46c82c2fcb305
PerseverIt <2934496502@qq.com>
2026-02-18 16:19:43 +0800

    add theme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,--启动立即加载
+
+    config = function()--setup函数放到里面
+     require("catppuccin").setup({
+    flavour = "latte", -- latte, frappe, macchiato, mocha
+    })
+
+	-- setup must be called before loading
+	vim.cmd.colorscheme "catppuccin"
+    end,
+  },
+}
